a9054147cce04a2bb922c87f6a8860b30e91c7b5
mzakocs <mzakocs@gmail.com>
2021-01-17 10:59:26 -0700

    Added fileName arg to scraper functions for easier separation

--- cargurus.py
+++ cargurus.py
@@ -5,65 +5,66 @@ from bs4 import BeautifulSoup
 from config import CarConfig
 import linker
 
-def scraper(link):
+def scraper(fileName):
     # Opens the CSV file for data storage
     # Uses w+ mode; will create the csv file if it doesn't exist
-    file = open("output/cargurus.csv", "a")
-    # Creates a GET request to the carGurus link in the config
-    site = requests.get(link)
-    # Parses the website using lxml
-    soup = BeautifulSoup(site.text, "lxml")
-    # Grabs the script tags, we're looking for the "featuredListings" object somewhere in the React code
-    scripts = soup.find_all("script")
-    # Loops through the script tags looking for the featuredListings object
-    for script in scripts:
-        # Checks if the script tag has any contents
-        if len(script.contents) <= 0:
-            continue
-        # Grabs the text from the script tag
-        text = script.contents[0]
-        # The script tag we're looking for is about 115,000
-        # in size on a small query, so just filter out anything below a certain threshold
-        if (len(text) > 50000):
-            # Finds the location of the listings object
-            startLocation = text.find('"featuredListings"') - 1
-            # Checks if it actually exists in this script tag
-            if startLocation:
-                # Goes back one index to include the starting {
-                # Finds the end of the listings object using the start of the next line
-                endLocation = text.find("\n        window.__PREFLIGHT__", startLocation) - 1
-                # Takes a substring to grab the entire object
-                listingsText = text[startLocation: endLocation]
-                # Converts from JSON text to a Python Dictionary
-                listings = json.loads(listingsText)["listings"]
-                # Starts putting data into CSV format
-                for listing in listings:
-                    # For Debug
-                    # print(json.dumps(listing, sort_keys=True, indent=4))
-                    # Grabs the year
-                    year = listing["carYear"]
-                    # Grabs the make
-                    #make = listing["makeName"]
-                    # Grabs the model
-                    #model = listing["modelName"]
-                    # Creates a Year Make Model string
-                    #ymm = formatUtil.createYMMString(year, make, model)
-                    # Grabs the price
-                    price = listing["expectedPriceString"].replace(",", "").replace("$", "") or "No Price"
-                    # Grabs the trim
-                    # trim = listing["trimName"] or "Not Listed"
-                    # Grabs the mileage
-                    miles = (listing["mileageString"] + " miles").replace(",", "").replace(" miles", "") or "Not Listed"
-                    # Grabs the location, have to append a couple entries to make this the same format as truecar
-                    #location = formatUtil.createLocationString(listing["distance"], listing["sellerCity"].split(", ")[0])
-                    # Grabs the color. Unfortunately CarGurus does not provide interior color.
-                    #color = formatUtil.createColorString(listing["normalizedExteriorColor"], "Unknown")
-                    # Puts the data into CSV format
-                    #csvString = (ymm + ", " + price + ", " + trim + ", " + miles + ", " + location + ", " + color)
-                    csvString = (str(year) + "," + price + "," + miles)
-                    # Finally writes the CSV data to the file
-                    print(csvString)
-                    file.write(csvString + "\n")
+    file = open(fileName, "a")
+    for link in linker.cargurus(config.getMake(), config.getModel(), config.getCity(), config.getState()):
+        # Creates a GET request to the carGurus link in the config
+        site = requests.get(link)
+        # Parses the website using lxml
+        soup = BeautifulSoup(site.text, "lxml")
+        # Grabs the script tags, we're looking for the "featuredListings" object somewhere in the React code
+        scripts = soup.find_all("script")
+        # Loops through the script tags looking for the featuredListings object
+        for script in scripts:
+            # Checks if the script tag has any contents
+            if len(script.contents) <= 0:
+                continue
+            # Grabs the text from the script tag
+            text = script.contents[0]
+            # The script tag we're looking for is about 115,000
+            # in size on a small query, so just filter out anything below a certain threshold
+            if (len(text) > 50000):
+                # Finds the location of the listings object
+                startLocation = text.find('"featuredListings"') - 1
+                # Checks if it actually exists in this script tag
+                if startLocation:
+                    # Goes back one index to include the starting {
+                    # Finds the end of the listings object using the start of the next line
+                    endLocation = text.find("\n        window.__PREFLIGHT__", startLocation) - 1
+                    # Takes a substring to grab the entire object
+                    listingsText = text[startLocation: endLocation]
+                    # Converts from JSON text to a Python Dictionary
+                    listings = json.loads(listingsText)["listings"]
+                    # Starts putting data into CSV format
+                    for listing in listings:
+                        # For Debug
+                        # print(json.dumps(listing, sort_keys=True, indent=4))
+                        # Grabs the year
+                        year = listing["carYear"]
+                        # Grabs the make
+                        #make = listing["makeName"]
+                        # Grabs the model
+                        #model = listing["modelName"]
+                        # Creates a Year Make Model string
+                        #ymm = formatUtil.createYMMString(year, make, model)
+                        # Grabs the price
+                        price = listing["expectedPriceString"].replace(",", "").replace("$", "") or "No Price"
+                        # Grabs the trim
+                        # trim = listing["trimName"] or "Not Listed"
+                        # Grabs the mileage
+                        miles = (listing["mileageString"] + " miles").replace(",", "").replace(" miles", "") or "Not Listed"
+                        # Grabs the location, have to append a couple entries to make this the same format as truecar
+                        #location = formatUtil.createLocationString(listing["distance"], listing["sellerCity"].split(", ")[0])
+                        # Grabs the color. Unfortunately CarGurus does not provide interior color.
+                        #color = formatUtil.createColorString(listing["normalizedExteriorColor"], "Unknown")
+                        # Puts the data into CSV format
+                        #csvString = (ymm + ", " + price + ", " + trim + ", " + miles + ", " + location + ", " + color)
+                        csvString = (str(year) + "," + price + "," + miles)
+                        # Finally writes the CSV data to the file
+                        print(csvString)
+                        file.write(csvString + "\n")
     # Closes the csv file
     file.close()
 
@@ -72,8 +73,6 @@ if __name__ == '__main__':
     # Creates a config to grab info for the linker
     config = CarConfig()
     # Wipes the CSV file
-    with open("output/cargurus.csv", "w") as f:
-        f.close()
-    # Loops through all of the links provided by the linker and scrapes each page
-    for link in linker.cargurus(config.getMake(), config.getModel(), config.getCity(), config.getState()):
-        scraper(link)
+    formatUtil.fileWipe("output/cargurus.csv")
+    # Starts the scraper
+    scraper("output/cargurus.csv")
--- truecar.py
+++ truecar.py
@@ -2,38 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 from config import CarConfig
 import linker
+import formatUtil
 
-def scraper(url):
+def scraper(fileName):
     # Opens the CSV file for data storage
     # Uses w+ mode; will create the csv file if it doesn't exist
-    file = open("output/truecar.csv", "a")
+    file = open(fileName, "a")
     # Instantiates a Config object
     config = CarConfig()
-    # Creates a GET request to the trueCar link in the config
-    site = requests.get(url)
-    soup = BeautifulSoup(site.text, "lxml")
-    creamySoup = soup.find(class_="row row-2 margin-bottom-3")
-    # Finds all of the car info cards on the website
-    cards = creamySoup.find_all(class_="card-content vehicle-card-body")
-    # Loops through all of the info cards to gather data
-    for card in cards:
-        year = card.find(class_="vehicle-card-year font-size-1").contents[0]
-        try:
-            price = (card.find(class_="heading-3 margin-y-1 font-weight-bold")).contents[0].replace(",", "").replace("$", "")
-        except AttributeError:
-            price = "No Price"
-        try:
-            miles = str(card.find(class_="d-flex w-100 justify-content-between").contents[0])
-            miles = miles[miles.find("</svg>") + 6:miles.find("</div>"):]
-            miles = miles.replace("<!-- -->", "").replace(",", "").replace(" miles", "")
-        except AttributeError:
-            miles = "Not Listed"
+    for link in linker.truecar(config.getMake(), config.getModel(), config.getCity(), config.getState()):
+        # Creates a GET request to the trueCar link in the config
+        site = requests.get(link)
+        soup = BeautifulSoup(site.text, "lxml")
+        creamySoup = soup.find(class_="row row-2 margin-bottom-3")
+        # Finds all of the car info cards on the website
+        cards = creamySoup.find_all(class_="card-content vehicle-card-body")
+        # Loops through all of the info cards to gather data
+        for card in cards:
+            year = card.find(class_="vehicle-card-year font-size-1").contents[0]
+            try:
+                price = (card.find(class_="heading-3 margin-y-1 font-weight-bold")).contents[0].replace(",", "").replace("$", "")
+            except AttributeError:
+                price = "No Price"
+            try:
+                miles = str(card.find(class_="d-flex w-100 justify-content-between").contents[0])
+                miles = miles[miles.find("</svg>") + 6:miles.find("</div>"):]
+                miles = miles.replace("<!-- -->", "").replace(",", "").replace(" miles", "")
+            except AttributeError:
+                miles = "Not Listed"
 
-        print(year + "," + price + "," + miles)
+            print(year + "," + price + "," + miles)
 
-        data = year + "," + price + "," + miles
-        file.write(data)
-        file.write("\n")
+            data = year + "," + price + "," + miles
+            file.write(data)
+            file.write("\n")
     # Closes the CSV file after adding scraped data
     file.close()
 
@@ -42,8 +44,6 @@ if __name__ == "__main__":
     # Creates a config to grab info for the linker
     config = CarConfig()
     # Wipes the file
-    with open("output/truecar.csv", "w") as f:
-        f.close()
-    # Loops through all of the links provided by the linker and scrapes each page
-    for link in linker.truecar(config.getMake(), config.getModel(), config.getCity(), config.getState()):
-        scraper(link)
+    formatUtil.fileWipe("output/truecar.csv")
+    # Runs the scraper
+    scraper("output/truecar.csv")
