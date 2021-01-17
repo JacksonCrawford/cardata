f2487cb8ad026864f0971060deda8c50fa991e8b
JacksonCrawford <jacksoncrawford@protonmail.com>
2021-01-17 15:03:32 -0500

    Added mean value datapoints on plot

--- carFinder.jl
+++ carFinder.jl
@@ -20,6 +20,7 @@ function plotData()
     data = getData()
     elements = line * 3
 
+
     years = Int64[]
     prices = Int64[]
     mileage = Int64[]
@@ -53,31 +54,62 @@ function plotData()
         push!(mileage, miles)
     end
 
+    yearDict = Dict()
+
+    yearSet = Set(years)
+    priceSet = Set(prices)
+    mileageSet = Set(mileage)
+    #=years = collect(yearSet)
+    prices = collect(priceSet)
+    mileage = collect(mileageSet)=#
+
     priceData = DataFrame(X=prices, Y=years)
     olsPrice = lm(@formula(X ~ Y), priceData)
     linearFitP = predict(olsPrice)
 
+    meanPrices = Float64[]
+
+    for year in years
+        data = priceData[priceData.Y .== year, :X]
+        mean = sum(data) / length(data)
+        push!(meanPrices, mean)
+    end
+
     mileData = DataFrame(X=mileage, Y=years)
     olsMiles = lm(@formula(X ~ Y), mileData)
     linearFitM = predict(olsMiles)
+
+    meanMiles = Float64[]
+
+    for year in years
+        data = priceData[priceData.Y .== year, :X]
+        mean = sum(data) / length(data)
+        push!(meanMiles, mean)
+    end
+
+    #=for year in yearSet
+        data = mileData[mileData.Y .== year, :X]
+        mean = sum(data) / length(data)
+        #println(mean)
+    end=#
 
 
     plotlyjs()
 
     # Plotting Prices
     pricePlot = scatter(years, prices, title="Price of Porsche 911's",
-        xlabel="Year", ylabel="Price", legend=false)
+        xlabel="Year", ylabel="Price", legend=true)
     plot!(years, linearFitP)
+    scatter!(years, meanPrices)
 
     # Plotting Mileage
     milePlot = scatter(years, mileage, title="Mileage of Porsche 911's",
-        xlabel="Year", ylabel="Mileage", legend=false)
+        xlabel="Year", ylabel="Mileage", legend=true)
     plot!(years, linearFitM)
+    scatter!(years, meanMiles)
 
     # Plotting everything
-    plot(pricePlot, milePlot, layout=grid(2,1), size=(650,800), legend=false)
-
-    print(mean(priceData))
+    plot(pricePlot, milePlot, layout=grid(2,1), size=(650,800), legend=true)
 
 end
 
