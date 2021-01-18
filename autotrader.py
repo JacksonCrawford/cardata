480ed73b5a3c41ae0b86729e5488f36216b4f791
mzakocs <mzakocs@gmail.com>
2021-01-17 18:37:54 -0700

    Added autotrader support

---
+++ autotrader.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup, NavigableString
+from config import CarConfig
+import linker
+import formatUtil
+
+def scraper(fileName):
+    # Opens the CSV file for data storage
+    # Uses w+ mode; will create the csv file if it doesn't exist
+    file = open(fileName, "a")
+    # Creates a config to grab info for the linker
+    config = CarConfig()
+    for link in linker.autotrader(config.getMake(), config.getModel(), config.getCity(), config.getState()):
+        # Creates a GET request to the autotrader link in the config
+        site = requests.get(link)
+        soup = BeautifulSoup(site.text, "lxml")
+        creamySoup = soup.find(class_="col-xs-12 col-md-9")
+        # Finds all of the car info cards on the website
+        cards = creamySoup.find_all(class_="item-card row display-flex align-items-stretch flex-column")
+        # Loops through all of the info cards to gather data
+        for card in cards:
+            # Quickly grabs the year of the car
+            year = card.find(class_="text-bold text-size-400 text-size-sm-500 link-unstyled").contents[0].split(" ")[1]
+            # Grabs the price of the car
+            try:
+                # Checks if there is a layered secondary price
+                # New cars listed on autotrader have prices listed differently
+                price = (card.find(class_="first-price")).contents[0]
+                if type(price) == NavigableString:
+                    price = price.replace(",", "")
+                else:
+                    price = price.find(class_="text-bold").contents[0].replace(",", "")
+            except AttributeError:
+                price = "No Price"
+            # Finds the mileage of the car
+            try:
+                miles = card.find(class_="item-card-specifications")
+                miles = miles.find("div")
+                miles = miles.contents[0].replace(" miles", "").replace(",", "")
+            except AttributeError:
+                miles = "Not Listed"
+            # Formats the data into CSV format and writes it to the file
+            data = year + "," + price + "," + miles
+            file.write(data)
+            file.write("\n")
+    # Closes the CSV file after adding scraped data
+    file.close()
+    print("Done Scraping AutoTrader!")
+
+# Main Method
+if __name__ == "__main__":
+    # Wipes the file
+    formatUtil.fileWipe("output/autotrader.csv")
+    # Runs the scraper
+    scraper("output/autotrader.csv")
--- linker.py
+++ linker.py
@@ -5,6 +5,7 @@ import math
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 from formatUtil import getZip
 
 def truecar(make, model, city, state):
@@ -38,7 +39,7 @@ def cargurus(make, model, city, state):
     # Starts by instantiating the Selenium web driver
     driver = webdriver.Firefox()
     # Opens the cargurus homepage with the driver
-    driver.get('https://www.cargurus.com/')
+    driver.get("https://www.cargurus.com/")
     # Gets a random ZIP code with city and state because CarGurus only accepts ZIP for location
     zipCode = getZip(city, state)
     # Creates a dictionary with the arguments needed in JS
@@ -71,7 +72,7 @@ def edmunds(make, model, city, state):
     # Starts by instantiating the Selenium web driver
     driver = webdriver.Firefox()
     # Opens the edmunds homepage with the driver
-    driver.get('https://www.edmunds.com/')
+    driver.get("https://www.edmunds.com/")
     # Gets a random ZIP code with city and state because CarGurus only accepts ZIP for location
     zipCode = getZip(city, state)
     # Finds the Make Model search bar by class name
@@ -111,11 +112,31 @@ def edmunds(make, model, city, state):
         pages = math.ceil(maxEntries / 18)
     else:
         pages = 1
-    print(pages)
     # Creates the link list for the different pages
     linkList = [url]
     for num in range(2, pages + 1):
         linkList.append(url + "?pagenumber=" + str(num))
     # Returns the list for the scraper
     print("Done Creating URL For Edmunds!")
+    return linkList
+
+def autotrader(make, model, city, state):
+    # Gets a ZIP code with city
+    zipCode = getZip(city, state)
+    # Creates the URL
+    url = "https://www.kbb.com/cars-for-sale/all/" + make + "/" + model + "/" + city + "-" + state + "-" + zipCode + "?dma=&searchRadius=100&location=&marketExtension=include&isNewSearch=true&showAccelerateBanner=false&sortBy=relevance&numRecords=25"
+    # Grabs the max record count off the page
+    # AutoTrader does paging in the URL through record limiting and not pages
+    siteText = requests.get(url).text
+    soup = BeautifulSoup(siteText, "lxml")
+    maxRecordSpan = soup.find(class_="results-text-container")
+    maxRecordText = maxRecordSpan.contents[0].replace("+", "").replace(",", "")
+    maxRecordSplit = maxRecordText.split(" ")
+    maxRecordCount = int(maxRecordSplit[2])
+    # Generates a list of all the links to different result pages
+    linkList = [url]
+    for num in range(25, maxRecordCount, 25):
+        linkList.append(url + "&firstRecord=" + str(num))
+    # Finally returns the list
+    print("Done Creating URL For AutoTrader!")
     return linkList
