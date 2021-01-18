55c10d23ffb2f58a4ff4cc33b9af332c4b84ad96
JacksonCrawford <jacksoncrawford@protonmail.com>
2021-01-18 13:11:14 -0500

    Changed name

--- carData.jl
+++ carData.jl
@@ -54,59 +54,26 @@ function plotData()
         push!(mileage, miles)
     end
 
-    yearDict = Dict()
-
-    yearSet = Set(years)
-    priceSet = Set(prices)
-    mileageSet = Set(mileage)
-    #=years = collect(yearSet)
-    prices = collect(priceSet)
-    mileage = collect(mileageSet)=#
-
     priceData = DataFrame(X=prices, Y=years)
     olsPrice = lm(@formula(X ~ Y), priceData)
     linearFitP = predict(olsPrice)
-
-    meanPrices = Float64[]
-
-    for year in years
-        data = priceData[priceData.Y .== year, :X]
-        mean = sum(data) / length(data)
-        push!(meanPrices, mean)
-    end
 
     mileData = DataFrame(X=mileage, Y=years)
     olsMiles = lm(@formula(X ~ Y), mileData)
     linearFitM = predict(olsMiles)
 
-    meanMiles = Float64[]
-
-    for year in years
-        data = priceData[priceData.Y .== year, :X]
-        mean = sum(data) / length(data)
-        push!(meanMiles, mean)
-    end
-
-    #=for year in yearSet
-        data = mileData[mileData.Y .== year, :X]
-        mean = sum(data) / length(data)
-        #println(mean)
-    end=#
-
 
     plotlyjs()
 
     # Plotting Prices
-    pricePlot = scatter(years, prices, title="Price of Porsche 911's",
+    pricePlot = scatter(years, prices, title="Price of Audi Q7's",
         xlabel="Year", ylabel="Price", legend=true)
     plot!(years, linearFitP)
-    scatter!(years, meanPrices)
 
     # Plotting Mileage
-    milePlot = scatter(years, mileage, title="Mileage of Porsche 911's",
+    milePlot = scatter(years, mileage, title="Mileage of Audi Q7's",
         xlabel="Year", ylabel="Mileage", legend=true)
     plot!(years, linearFitM)
-    scatter!(years, meanMiles)
 
     # Plotting everything
     plot(pricePlot, milePlot, layout=grid(2,1), size=(650,800), legend=true)
