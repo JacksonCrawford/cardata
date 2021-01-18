000b6f52893313951bdfaa73295a023b15aafa44
mzakocs <mzakocs@gmail.com>
2021-01-17 18:38:04 -0700

    Added autotrader to auto-scrape file

--- scraper.py
+++ scraper.py
@@ -2,12 +2,17 @@ import formatUtil
 import truecar
 import cargurus
 import edmunds
+import autotrader
 
 # Runs all of the scrapers into master.csv
 if __name__ == "__main__":
+    # Stores the master CSV file path/name
+    outputFilename = "output/master.csv"
     # Wipes the file
-    formatUtil.fileWipe("output/master.csv")
+    formatUtil.fileWipe(outputFilename)
     # Activates all the scrapers
-    truecar.scraper("output/master.csv")
-    cargurus.scraper("output/master.csv")
-    edmunds.scraper("output/master.csv")
+    truecar.scraper(outputFilename)
+    cargurus.scraper(outputFilename)
+    edmunds.scraper(outputFilename)
+    autotrader.scraper(outputFilename)
+    print("Completely Finished!")
