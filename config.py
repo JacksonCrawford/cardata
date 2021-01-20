42d957f58761152c05c0e603e4774213d7729970
mzakocs <mzakocs@gmail.com>
2021-01-20 16:00:09 -0700

    Added terminal config changer

--- config.py
+++ config.py
@@ -8,6 +8,7 @@
 import configparser
 import os
 
+
 class CarConfig:
 
     def __init__(self):
@@ -15,7 +16,7 @@ class CarConfig:
         self.config = configparser.ConfigParser()
         if not os.path.exists('config.ini'):
             self.config['Search Criteria'] = {'make': '', 'model': '',
-                                           'city': '', 'stateCode': ''}
+                                              'city': '', 'stateCode': ''}
             with open('config.ini', 'w+') as configfile:
                 self.config.write(configfile)
             print("Please fill out the config file and restart the application!")
@@ -27,6 +28,38 @@ class CarConfig:
         self._model = self.config['Search Criteria']['model']
         self._city = self.config['Search Criteria']['city']
         self._state = self.config['Search Criteria']['stateCode']
+
+    def promptConfigChange(self):
+        # Allows the user to change the config in the terminal before running the scrapers
+        print("\nCarData Scraper by Jackson C. and Mitch Z.")
+        print("Input New Config Values Below. Press Enter/Return To Use The Current Config Value.\n")
+        # Grabs the make or uses the current make if nothing is inputted
+        print("Car Make (ex. Toyota): ", end="")
+        make = input()
+        if (len(make) == 0):
+            make = self.getMake()
+        # Grabs the model or uses the current model if nothing is inputted
+        print("Car Model (ex. Camry): ", end="")
+        model = input()
+        if (len(model) == 0):
+            make = self.getModel()
+        # Grabs the city or uses the current city if nothing is inputted
+        print("City (ex. Charlotte): ", end="")
+        city = input()
+        if (len(city) == 0):
+            city = self.getCity()
+        # Grabs the state code or uses the current state if nothing is inputted
+        print("State Code (ex. NC): ", end="")
+        state = input()
+        if (len(state) == 0):
+            state = self.getState()
+        print("\n", end="")
+        # Puts the new values into the config object
+        self.config['Search Criteria'] = {'make': make, 'model': model,
+                                          'city': city, 'stateCode': state}
+        # Writes the values to the config file
+        with open('config.ini', 'w+') as configFile:
+            self.config.write(configFile)
 
     def getMake(self):
         # Get method for the link to Car Gurus
@@ -43,4 +76,3 @@ class CarConfig:
     def getState(self):
         # Get method for the link to TrueCar
         return self._state.lower()
-
--- scraper.py
+++ scraper.py
@@ -3,9 +3,12 @@ import truecar
 import cargurus
 import edmunds
 import autotrader
+import config
 
 # Runs all of the scrapers into master.csv
 if __name__ == "__main__":
+    # Prompts the user to change the config if they want
+    config.CarConfig().promptConfigChange()
     # Stores the master CSV file path/name
     outputFilename = "output/master.csv"
     # Wipes the file
