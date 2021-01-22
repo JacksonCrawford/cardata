7bd51bd71d5d62c51f579cfcf7c85e1e591da4d5
mzakocs <mzakocs@gmail.com>
2021-01-22 11:28:24 -0700

    Added cars.com support

---
+++ carsdotcom.py
@@ -0,0 +1,50 @@
+import requests
+import formatUtil
+import linker
+import json
+from config import CarConfig
+
+def scraper(fileName):
+    # Creates a CarConfig object for search criteria
+    config = CarConfig()
+    # Opens the CSV file for appending, will likely be wiped already
+    file = open(fileName, "a")
+    # Loops through each link provided by the linker and scrapes the site
+    for link in linker.carsdotcom(config.getMake(), config.getModel(), config.getCity(), config.getState()):
+        # Makes a request to the edmunds site for the source code
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
+        site = requests.get(link, headers=headers)
+        # Converts the data to JSON for easy digestion
+        jsonData = json.loads(site.text)
+        # Grabs car results from json
+        results = jsonData["dtm"]["vehicle"]
+        # Loops through each result
+        for result in results:
+            # Parses the year
+            try:
+                year = str(result["year"])
+            except Exception:
+                continue
+            # Parses the price data
+            try:
+                price = str(round(result["price"]))
+            except Exception:
+                continue
+            # Parses mileage
+            try:
+                miles = str(result["mileage"])
+            except Exception:
+                continue
+            # Puts data into CSV format
+            data = year + "," + price + "," + miles
+            # Writes the data to CSV file
+            file.write(data)
+            file.write("\n")
+    file.close()
+    print ("Done Scraping Cars.com!")
+
+if __name__ == "__main__":
+    # Wipes the CSV file
+    formatUtil.fileWipe("output/carsdotcom.csv")
+    # Runs the scraper
+    scraper("output/carsdotcom.csv")
